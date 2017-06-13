26f93ca906dbc68ff9ccaacaa7557ba96cfca714
Jasper Bok <hello@jasperbok.nl>
2017-06-13 22:46:06 +0200

    Added the UserType object

--- togglnim.nim
+++ togglnim.nim
@@ -11,6 +11,54 @@ const TogglApiUrl: string = "https://www.toggl.com/api/v8"
 var togglApiToken: string = ""
 
 
+type
+  TogglUser* = ref object of RootObj
+    ## An object wrapper around a Toggl userprofile.
+    apiToken*: string
+    defaultWid*: uint
+    email*: string
+    fullname*: string
+    jqueryTimeOfDayFormat*: string
+    jqueryDateFormat*: string
+    timeOfDayFormat*: string
+    dateFormat*: string
+    storeStartAndStopTime*: bool
+    beginningOfWeek*: uint
+    language*: string
+    imageUrl*: string
+    sidebarPiechart*: bool
+    sendProductEmails*: bool
+    sendWeeklyReport*: bool
+    sendTimerNotifications*: bool
+    openIdEnabled*: bool
+    timezone*: string
+
+
+proc newTogglUser(data: JsonNode): TogglUser =
+  ## Create a new TogglUser instance from Json data as returned by
+  ## the Toggl API.
+  TogglUser(
+    apiToken: data["api_token"].getStr(),
+    defaultWid: uint(data["default_wid"].getNum()),
+    email: data["email"].getStr(),
+    fullname: data["fullname"].getStr(),
+    jqueryTimeOfDayFormat: data["jquery_timeofday_format"].getStr(),
+    jqueryDateFormat: data["jquery_date_format"].getStr(),
+    timeOfDayFormat: data["timeofday_format"].getStr(),
+    dateFormat: data["date_format"].getStr(),
+    storeStartAndStopTime: data["store_start_and_stop_time"].getBVal(),
+    beginningOfWeek: uint(data["beginning_of_week"].getNum()),
+    language: data["language"].getStr(),
+    imageUrl: data["image_url"].getStr(),
+    sidebarPiechart: data["sidebar_piechart"].getBVal(),
+    sendProductEmails: data["send_product_emails"].getBVal(),
+    sendWeeklyReport: data["send_weekly_report"].getBVal(),
+    sendTimerNotifications: data["send_timer_notifications"].getBVal(),
+    openIdEnabled: data["openid_enabled"].getBVal(),
+    timezone: data["timezone"].getStr()
+  )
+
+
 proc configureTogglApiToken*(token: string) =
   ## Sets the Toggl API token to use.
   ##
